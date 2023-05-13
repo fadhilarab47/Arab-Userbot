ac33ca525b4b357190c9ce5fa14b0a371a2119a7
iamuput <yogaaasp73@gmail.com>
2023-05-13 12:48:06 +0700

    Update config.py

--- config.py
+++ config.py
@@ -16,34 +16,34 @@ from dotenv import load_dotenv
 load_dotenv(".env")
 
 
-ALIVE_EMOJI = getenv("ALIVE_EMOJI", "👑")
-ALIVE_LOGO = getenv("ALIVE_LOGO", "https://telegra.ph/file/d370f45bf3ff8fa0cba8f.jpg")
-ALIVE_TEKS_CUSTOM = getenv("ALIVE_TEKS_CUSTOM", "Hey bro, I am rams.")
+ALIVE_EMOJI = getenv("ALIVE_EMOJI", "🥵")
+ALIVE_LOGO = getenv("ALIVE_LOGO", "https://telegra.ph/file/b42b7a4a22ba89287cad4.jpg")
+ALIVE_TEKS_CUSTOM = getenv("ALIVE_TEKS_CUSTOM", "Hi, I'm Uputt-Project.")
 API_HASH = getenv("API_HASH", "34efb38c74d5e6b25d1bb6234396a8af")
 API_ID = getenv("API_ID", "23129036")
 BLACKLIST_CHAT = getenv("BLACKLIST_CHAT", None)
 if not BLACKLIST_CHAT:
-    BLACKLIST_CHAT = [-1001692751821]
+    BLACKLIST_CHAT = [-1001608701614]
 BLACKLIST_GCAST = {int(x) for x in getenv("BLACKLIST_GCAST", "").split()}
 BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 BOT_VER = "1.0.7"
 BRANCH = getenv("BRANCH", "main")
-CH_SFS = getenv("CH_SFS", "b4c0d")
-IG_ALIVE = getenv("IG_ALIVE", "e_rama11")
-CHANNEL = getenv("CHANNEL", "userbotch")
+CH_SFS = getenv("CH_SFS", "amneseey0u")
+IG_ALIVE = getenv("IG_ALIVE", "syhptraaa.__")
+CHANNEL = getenv("CHANNEL", "amneseey0u")
 CMD_HANDLER = getenv("CMD_HANDLER", ".")
 CMD_HNDLR = CMD_HANDLER
-ID_OWNER = getenv("ID_OWNER", "910766621")
+ID_OWNER = getenv("ID_OWNER", "1912667035")
 DB_URL = getenv("DATABASE_URL", "")
 GIT_TOKEN = getenv("GIT_TOKEN", "")
-GROUP = getenv("GROUP", "GeezRam")
+GROUP = getenv("GROUP", "UputtSupport")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 PMPERMIT_PIC = getenv("PMPERMIT_PIC", None)
 OPENAI_API_KEY = getenv("OPENAI_API_KEY", "")
 PM_AUTO_BAN = strtobool(getenv("PM_AUTO_BAN", "True"))
-REPO_URL = getenv("REPO_URL", "https://github.com/GeezRampy/Ram-Pyro")
+REPO_URL = getenv("REPO_URL", "https://github.com/iamuput/Uputt-Project")
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
