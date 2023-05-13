7ae98c561241692dec4b9e92af71deeb82b19b27
iamuput <yogaaasp73@gmail.com>
2023-05-13 16:42:53 +0700

    Update alive.py

--- rams/modules/alive.py
+++ rams/modules/alive.py
@@ -52,7 +52,7 @@ async def alive(client: Client, message: Message):
         f"{emoji} <b>Pyrogram Version :</b> <code>{versipyro}</code> \n"
         f"╼┅━━━━━━━━━━╍━━━━━━━━━━┅╾\n"
         f" 𝙼𝚈 𝙼𝙰𝚂𝚃𝙴𝚁 : [CLICK](tg://user?id={client.me.id}) \n\n"
-        f"    **[𝚂𝚞𝚙𝚙𝚘𝚛𝚝](https://t.me/{GROUP})** | **[𝙲𝚑𝚊𝚗𝚗𝚎𝚕](https://t.me/{CHANNEL})** | **[𝙾𝚠𝚗𝚎𝚛](t.me/iamupt)**"
+        f"    **[𝚂𝚞𝚙𝚙𝚘𝚛𝚝](https://t.me/{GROUP})** | **[𝙲𝚑𝚊𝚗𝚗𝚎𝚕](https://t.me/{CHANNEL})** | **[𝙾𝚠𝚗𝚎𝚛](t.me/iamuput)**"
     )
     try:
         await asyncio.gather(
