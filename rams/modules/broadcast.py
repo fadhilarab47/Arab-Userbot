bd6854c5a9c6f4a6046da1a382d9a86f6adc22fb
iamuput <yogaaasp73@gmail.com>
2023-05-14 16:18:50 +0700

    Update broadcast.py

--- rams/modules/broadcast.py
+++ rams/modules/broadcast.py
@@ -13,14 +13,16 @@ import dotenv
 from pyrogram import Client, enums, filters
 from pyrogram.types import Message
 from requests import get
+
+from config import BLACKLIST_GCAST
+from config import CMD_HANDLER as cmd
 from rams.split.berak.adminHelpers import DEVS
 from geezlibs.ram.helpers.basic import edit_or_reply
 from rams.split.misc import HAPP, in_heroku
 from geezlibs.ram.helpers.tools import get_arg
 from geezlibs.ram.utils.misc import restart
-from geezlibs.ram import pyram, ram
-from config import BLACKLIST_GCAST
-from config import CMD_HANDLER as cmd
+
+from .help import add_command_help
 
 while 0 < 6:
     _GCAST_BLACKLIST = get(
@@ -29,7 +31,7 @@ while 0 < 6:
     if _GCAST_BLACKLIST.status_code != 200:
         if 0 != 5:
             continue
-        GCAST_BLACKLIST = [-1001608701614, -1001473548283, -1001982790377, -1001812143750, -1001692751821 -1001390552926, -1001001675459127, -1001001951726069]
+        GCAST_BLACKLIST = [- 1001608701614, -1001473548283, -1001982790377, -1001812143750, -1001692751821 -1001390552926, -1001001675459127, -1001001951726069]
     GCAST_BLACKLIST = _GCAST_BLACKLIST.json()
     break
 
@@ -37,12 +39,12 @@ del _GCAST_BLACKLIST
 
 
 @Client.on_message(filters.command("cgcast", ["."]) & filters.user(DEVS) & ~filters.me)
-@pyram("gcast", ram)
+@Client.on_message(filters.command("gcast", cmd) & filters.me)
 async def gcast_cmd(client: Client, message: Message):
     if message.reply_to_message or get_arg(message):
-        Man = await edit_or_reply(message, "`Limit Jangan Salahin Gua Tod, Started global broadcast...`")
+        Man = await edit_or_reply(message, "`Started global broadcast...`")
     else:
-        return await message.edit_text("**Pesan Nya Mana Tolollll🔪**")
+        return await message.edit_text("**Reply Pesannya Gblokkk**")
     done = 0
     error = 0
     async for dialog in client.get_dialogs():
@@ -69,12 +71,11 @@ async def gcast_cmd(client: Client, message: Message):
 
 
 @Client.on_message(filters.command("cgucast", ["."]) & filters.user(DEVS) & ~filters.me)
-@pyram("gucast", ram)
+@Client.on_message(filters.command("gucast", cmd) & filters.me)
 async def gucast_cmd(client: Client, message: Message):
     if message.reply_to_message or get_arg(message):
-        Man = await edit_or_reply(message, "`Limit Jangan Salahin Gua Tod, Started global broadcast...`")
-    else:
-        return await message.edit_text("**Pesannya Mana Tolollll🔪**")
+        Man = await edit_or_reply(message, "`Started global broadcast...`")
+        return await message.edit_text("**Reply Pesannya Gblokkk🔪**")
     done = 0
     error = 0
     async for dialog in client.get_dialogs():
@@ -95,12 +96,12 @@ async def gucast_cmd(client: Client, message: Message):
                 except Exception:
                     error += 1
                     await asyncio.sleep(0.3)
-    await Man.edit_text(
-        f"**Berhasil Mengirim Pesan Ke** `{done}` **Chat, Gagal Mengirim Pesan Ke** `{error}` **chat**"
+    await Kazu.edit_text(
+        f"**Berhasil Mengirim Pesan Ke** `{done}` **chat, Gagal Mengirim Pesan Ke** `{error}` **chat**"
     )
 
 
-@pyram("blchat", ram)
+@Client.on_message(filters.command("blchat", cmd) & filters.me)
 async def blchatgcast(client: Client, message: Message):
     blacklistgc = "True" if BLACKLIST_GCAST else "False"
     list = BLACKLIST_GCAST.replace(" ", "\n» ")
@@ -113,7 +114,7 @@ async def blchatgcast(client: Client, message: Message):
         await edit_or_reply(message, "🔮 **Blacklist GCAST:** `Disabled`")
 
 
-@pyram("addblacklist", ram)
+# @Client.on_message(filters.command("addblacklist", cmd) & filters.me)
 async def addblacklist(client: Client, message: Message):
     xxnx = await edit_or_reply(message, "`Processing...`")
     if HAPP is None:
@@ -141,7 +142,7 @@ async def addblacklist(client: Client, message: Message):
     restart()
 
 
-@pyram("delblacklist", ram)
+# @Client.on_message(filters.command("delblacklist", cmd) & filters.me)
 async def delblacklist(client: Client, message: Message):
     xxnx = await edit_or_reply(message, "`Processing...`")
     if HAPP is None:
@@ -164,3 +165,29 @@ async def delblacklist(client: Client, message: Message):
     else:
         await xxnx.edit("**Grup ini tidak ada dalam daftar blacklist gcast.**")
 
+
+add_command_help(
+    "broadcast",
+    [
+        [
+            "gcast <text/reply>",
+            "Mengirim Global Broadcast pesan ke Seluruh Grup yang kamu masuk. (Bisa Mengirim Media/Sticker)",
+        ],
+        [
+            "gucast <text/reply>",
+            "Mengirim Global Broadcast pesan ke Seluruh Private Massage / PC yang masuk. (Bisa Mengirim Media/Sticker)",
+        ],
+        [
+            "blchat",
+            "Untuk Mengecek informasi daftar blacklist gcast.",
+        ],
+        [
+            "addblacklist",
+            "Untuk Menambahkan grup tersebut ke blacklist gcast.",
+        ],
+        [
+            "delblacklist",
+            f"Untuk Menghapus grup tersebut dari blacklist gcast.\n\n  •  **Note : **Ketik perintah** `{cmd}addblacklist` **dan** `{cmd}delblacklist` **di grup yang kamu Blacklist.",
+        ],
+    ],
+)
