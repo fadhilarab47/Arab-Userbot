eb2e65965c670a5ac3f5ef77a2fe414c63f464bc
iamuput <yogaaasp73@gmail.com>
2023-05-17 02:25:56 +0700

    Update rhelp.py

--- rams/modules/rhelp.py
+++ rams/modules/rhelp.py
@@ -4,7 +4,7 @@ from .help import add_command_help
 add_command_help(
     "dm",
     [
-        ["dm", "mengirim pesan ke chat pribadi (contoh: `.dm @thisrama Hii`."],
+        ["dm", "mengirim pesan ke chat pribadi (contoh: `.dm @iamuput Hii`."],
     ],
 )
 
